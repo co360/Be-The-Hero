f76025d3337a4fb67d17471a87298e7273566fc5
Maycon Santos <maycon.s.santos44@gmail.com>
2020-03-28 00:24:37 -0300

    Front-End e Back-End da aplicação.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes'
+import './global.css';
+import { Route } from 'react-router-dom';
+
+//JSX JavaScript e XML
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
